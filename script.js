babad87283fe58192085d6faace8bf84c5e13f02
sjcb1 <129980961+sjcb1@users.noreply.github.com>
2023-08-17 17:05:04 +0000

    Streamline the code for the winning statements

--- script.js
+++ script.js
@@ -8,21 +8,15 @@ let randomNum2 = Math.floor(Math.random() *6) +1;
 let randomImgSrc2 ="./images/dice" + randomNum2 +".png";
 let d2 = document.querySelectorAll("img")[1].setAttribute("src", randomImgSrc2);
 
-//winning statement
-const p1Win = "Player 1, You WIN!!";
-const p2Win = "Player 2, You WIN!!";
-const draw = "Its a Draw. GO Again";
-
-let title = document.querySelector("h1");
-
+//winning statements
 if (randomNum1 > randomNum2){
-  title.innerText = p1Win
+  document.querySelector("h1").innerText = "Player 1, You WIN!!"
 }
-
+  
 else if (randomNum2 > randomNum1) {
- title.innerText = p2Win 
+  document.querySelector("h1").innerText = "Player 2, You WIN!!"
 }
 
 else {
-  title.innerText = draw
-}  
+  document.querySelector("h1").innerText = "Its's a DRAW. Go Again!!"
+}
